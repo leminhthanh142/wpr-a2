f61c5619ad34f5f0fe419bb8c17e58172d40c19e
leminhthanh142 <thanhle12345789@gmail.com>
2021-11-13 22:36:34 +0700

    :DD

--- flashcards-oop-custom_event/js/main.js
+++
@@ -1,5 +0,0 @@
-const cardContainer = document.querySelector('#flashcard-container');
-const statusBarContainer = document.querySelector('#status-bar');
-
-
-const app = new App(cardContainer, statusBarContainer, WORDS);
